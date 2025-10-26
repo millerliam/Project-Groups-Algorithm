80d08d487d973e90704af0dd927e38db5cfa703e
millerliam <miller.lia@northeastern.edu>
2025-10-26 04:26:01 -0400

    Create Utilities.hpp

---
+++ Utilities.hpp
@@ -0,0 +1,9 @@
+#ifndef UTILITIES_HPP
+#define UTILITIES_HPP
+
+#include <string>
+
+std::string toLowerCase(const std::string& str);
+int convertSkillLevel(const std::string& skill);
+
+#endif // UTILITIES_HPP
